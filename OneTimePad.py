d15b213b58303b14ff2e995cd09e8e812b3121b8
PauL <57185255+paulh-afk@users.noreply.github.com>
2023-07-13 19:46:16 +0200

    init OneTimePad class

---
+++ OneTimePad.py
@@ -0,0 +1,12 @@
+class OneTimePad:
+    """_summary_
+    """
+
+    def __init__(self, key: str, message: str) -> None:
+        pass
+
+    def encrypt(self) -> str:
+        pass
+
+    def decrypt(self) -> str:
+        pass
